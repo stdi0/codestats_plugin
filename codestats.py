f829a27b54c83928a70a4967ea7056311a12d9ae
Denis <denisyurievich94@gmail.com>
2017-03-25 17:53:01 +0300

    update

---
+++ codestats.py
@@ -0,0 +1,45 @@
+import sublime
+import sublime_plugin
+import threading
+import urllib.request
+import urllib.parse
+import os
+import re
+
+
+class CodestatsEvent(sublime_plugin.EventListener):
+
+    count = 0
+
+    def on_modified(self, view):
+        self.count += 1
+        if self.count > 10:
+            thread = CodestatsApiCall(self.count)
+            thread.start()
+            self.count = 0
+        print(self.count)
+
+class CodestatsApiCall(threading.Thread):
+    def __init__(self, count):
+        self.count = count
+        threading.Thread.__init__(self)
+
+    def run(self):
+        path = os.path.dirname(__file__)
+        path = re.sub("codestats.py", '', path)
+        path = path + "/username.txt"
+        print(path)
+        try:
+            f = open(path)
+            line = f.readlines()
+            username = re.sub("^\s+|\n|\r|\s+$", '', line[0])
+            password = re.sub("^\s+|\n|\r|\s+$", '', line[1])
+        except:
+            sublime.error_message('Username file error.')
+        #print(username + 'test')
+        try:
+            data = urllib.parse.urlencode({'count': self.count, 'password': password}).encode()
+            request = urllib.request.Request('http://codestats.pythonanywhere.com/' + username + '/api_call/', data=data)
+            resp = urllib.request.urlopen(request)
+        except:
+            sublime.error_message('Connection error.')
